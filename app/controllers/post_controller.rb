6bd6c591faf48033e8e58d96e773d3ee820546eb
kateconnaughton <kate.connaughton@kellogg.northwestern.edu>
2023-02-21 22:42:19 +0000

    take6

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,14 +1,11 @@
 class PostController < ApplicationController
 
-def show
-    @post = Posts.find_by({ "id" => @post["id"]})
-    @place = Places.find_by({ "id" => @post["place_id"]})
+def index
+    @posts = Post.all
 end
 
 def new
     @post = Post.new
-    @place = Place.find_by({ "id" => params["place_id"]})
-    @post["place_id"] = @place["id"]
 end
 
 def create
