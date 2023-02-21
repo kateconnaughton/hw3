7293b5cf287f3698df6b4eea7d26db74419fe0cf
kateconnaughton <kate.connaughton@kellogg.northwestern.edu>
2023-02-21 05:45:22 +0000

    take2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,22 @@
 class PlacesController < ApplicationController
+
+    def index
+        @place = Places.all
+    end
+    
+    def show
+        @place = Places.find_by({ "id" => params["id"]})
+    end
+
+    def new
+        @place = Places.new
+    end
+
+    def create
+        @place = Places.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+    end
+
 end
